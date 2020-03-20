b5843da740dcfba69e41e2767823cc2165fc5eb1
Michal Kruszewski <kruszewski2@gmail.com>
2020-03-20 13:47:56 +0100

    js: added local storage integration

--- src/js/main.js
+++ src/js/main.js
@@ -17,10 +17,25 @@ const glassCounter = document.querySelector('.glass__counter--js');
 const navigationAdd = document.querySelector('.navigation__add--js');
 const navigationRemove = document.querySelector('.navigation__remove--js');
 
+const key = new Date().toISOString().slice(0, 10);
+
+window.addEventListener('load', function(){
+  let counter = 0;
+  
+  if(localStorage.getItem(key))
+    counter = localStorage.getItem(key);
+  else 
+    localStorage.setItem(key, counter);
+
+  glassCounter.innerHTML = counter;
+});
+
 navigationAdd.addEventListener('click', function(e){
   let counter = parseInt(glassCounter.innerHTML);
   counter += 1;
   glassCounter.innerHTML = counter;
+  
+  localStorage.setItem(key, counter);
 });
 
 navigationRemove.addEventListener('click', function(e){
@@ -28,4 +43,7 @@ navigationRemove.addEventListener('click', function(e){
   if(counter > 0)
     counter-=1;
   glassCounter.innerHTML = counter;
+
+  if(localStorage.getItem(key))
+    localStorage.setItem(key, counter);
 });
